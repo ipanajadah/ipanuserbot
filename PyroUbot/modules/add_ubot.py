8c7156f47d94dad837e0999d55e013da9bd3b1e8
ipan1434 <flaelva6@gmail.com>
2025-04-03 17:41:48 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -16,8 +16,12 @@ from PyroUbot import *
 async def _(client, message):
     buttons = BTN.START(message)
     msg = MSG.START(message)
-    await message.reply(msg, reply_markup=InlineKeyboardMarkup(buttons))
 
+    await message.reply_photo(
+        photo="https://files.catbox.moe/pdnxpx.jpg", 
+        caption=msg, 
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
 
 @PY.CALLBACK("bahan")
 async def _(client, callback_query):
