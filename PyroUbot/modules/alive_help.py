7100f1bab97a2ffab9981e26b83d5f5605c9bd94
ipan1434 <flaelva6@gmail.com>
2025-04-08 11:54:27 +0700

    Update alive_help.py

--- PyroUbot/modules/alive_help.py
+++ PyroUbot/modules/alive_help.py
@@ -156,7 +156,7 @@ async def user_help(client, message):
 @PY.INLINE("^user_help")
 async def user_help_inline(client, inline_query):
     SH = await ubot.get_prefix(inline_query.from_user.id)
-    msg = f"""<blockquote><b>𝗛 𝗘 𝗟 𝗣 𝗠 𝗘 𝗡 𝗨</b>
+    msg = f"""<blockquote><b>🤓𝗛 𝗘 𝗟 𝗣 𝗠 𝗘 𝗡 𝗨</b>
 <b> ∘ ᴜsᴇʀ: <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a></b>
 <b> ∘ ᴏᴡɴᴇʀ: @IPANNEWERA</b>
 <b> ∘ ᴘʟᴜɢɪɴs: {len(HELP_COMMANDS)}</b>
@@ -185,7 +185,7 @@ async def help_callback(client, callback_query):
     tutup_match = re.match(r"help_tutup\((.+?)\)", callback_query.data)
     back_match = re.match(r"help_back", callback_query.data)
     SH = await ubot.get_prefix(callback_query.from_user.id)
-    top_text = f"""<blockquote><b>𝗛 𝗘 𝗟 𝗣 𝗠 𝗘 𝗡 𝗨</b>
+    top_text = f"""<blockquote><b>🤓𝗛 𝗘 𝗟 𝗣 𝗠 𝗘 𝗡 𝗨</b>
 <b> ∘ ᴜsᴇʀ: <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a></b>
 <b> ∘ ᴏᴡɴᴇʀ: @IPANNEWERA</b>
 <b> ∘ ᴘʟᴜɢɪɴs: {len(HELP_COMMANDS)}</b>
