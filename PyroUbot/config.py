c8f9de1ee9f352ab874a6f2ffde11731e6f7c77b
ipan1434 <flaelva6@gmail.com>
2025-04-10 15:54:49 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -15,10 +15,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "7915079265:AAFyj0V5G2n7vwZ0ZyhlYuu8OOs-Do4oM
 
 OWNER_ID = int(os.getenv("OWNER_ID", "5937391590"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002419662880").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002566228411").split()))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Ipanndongok:Ipanndongok@ipanndongok.bg7xf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", " -1002614879090"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002566228411"))
