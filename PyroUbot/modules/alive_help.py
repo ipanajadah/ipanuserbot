03b7e06beb4b02b644ddb2113c63eab4c510af3b
ipan1434 <flaelva6@gmail.com>
2025-04-16 23:46:00 +0700

    Update alive_help.py

--- PyroUbot/modules/alive_help.py
+++ PyroUbot/modules/alive_help.py
@@ -156,8 +156,9 @@ async def user_help(client, message):
 @PY.INLINE("^user_help")
 async def user_help_inline(client, inline_query):
     SH = await ubot.get_prefix(inline_query.from_user.id)
-    msg = f"""<b> ∘ ᴜsᴇʀ: <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a></b>
-<b> ∘ ᴘʀᴇꜰɪxᴇs: {' '.join(SH)}</b>"""
+    msg = f"""<b> 𝐇 𝐄 𝐋 𝐏 𝐌 𝐄 𝐍 𝐔
+<b> ∘ ᴘʀᴇꜰɪxᴇs: {' '.join(SH)}</b>
+<b> ∘ ᴜsᴇʀ: <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a></b>"""
     results = [InlineQueryResultArticle(
         title="Help Menu!",
         reply_markup=InlineKeyboardMarkup(paginate_modules(0, HELP_COMMANDS, "help")),
@@ -182,8 +183,9 @@ async def help_callback(client, callback_query):
     tutup_match = re.match(r"help_tutup\((.+?)\)", callback_query.data)
     back_match = re.match(r"help_back", callback_query.data)
     SH = await ubot.get_prefix(callback_query.from_user.id)
-    top_text = f"""<b> ∘ ᴜsᴇʀ: <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a></b>
-<b> ∘ ᴘʀᴇꜰɪxᴇs: {' '.join(SH)}</b>"""
+    top_text = f"""<b> 𝐇 𝐄 𝐋 𝐏 𝐌 𝐄 𝐍 𝐔
+<b> ∘ ᴘʀᴇꜰɪxᴇs: {' '.join(SH)}</b>
+<b> ∘ ᴜsᴇʀ: <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a></b>"""
 
     if mod_match:
         module = (mod_match.group(1)).replace(" ", "_")
