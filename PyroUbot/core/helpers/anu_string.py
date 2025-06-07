1e07089cc2680b7070caa9542c4858e32c395d6f
Not faund <flaelva6@gmail.com>
2025-06-08 02:20:11 +0700

    Update anu_string.py

--- PyroUbot/core/helpers/anu_string.py
+++ PyroUbot/core/helpers/anu_string.py
@@ -3,13 +3,13 @@ from PyroUbot import *
 class STR:
     async def PONG(client):
         str_pong = await get_vars(client.me.id, "STRING_PONG")
-        string_pong = str_pong if str_pong else "± ᴘᴏɴɢ"
+        string_pong = str_pong if str_pong else "Pong"
         result = f"{string_pong}"
         return result
 
     async def OWNER(client):
         str_pong = await get_vars(client.me.id, "STRING_OWNER")
-        string_pong = str_pong if str_pong else "± ᴏᴡɴᴇʀ"
+        string_pong = str_pong if str_pong else "Owner"
         result = f"{string_pong}"
         return result
 
