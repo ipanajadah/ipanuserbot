57ee4955a959c37223f72ef7fd93e1b0d2014c7e
Not faund <flaelva6@gmail.com>
2025-06-08 22:22:31 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -47,8 +47,8 @@ async def _(client, message):
     buttons = BTN.START(message)
     msg = MSG.START(message)
 
-    await message.reply_photo() 
-        photo="https://files.catbox.moe/0k8f7l.jpg", 
+    await message.reply_photourl(
+        photourl="https://files.catbox.moe/0k8f7l.jpg", 
         caption=msg, 
         reply_markup=InlineKeyboardMarkup(buttons)
         
