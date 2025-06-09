dd06d4f45270c9ccccb637a35e1055fa751b73bf
Not faund <flaelva6@gmail.com>
2025-06-09 18:16:23 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -44,7 +44,7 @@ async def _(client, message):
     await asyncio.sleep(3.5)
     await xxx.delete()
 
-     photo="https://files.catbox.moe/1b37zw.jpg"
+     photo="https://files.catbox.moe/1b37zw.jpg",
     
     return await message.reply_photo(msg, reply_photo_markup=InlineKeyboardMarkup(buttons))
 
