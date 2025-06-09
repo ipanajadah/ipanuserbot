34f00ff3cb2d7fef45ade8f834d7f084e776cad5
Not faund <flaelva6@gmail.com>
2025-06-09 17:59:13 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -43,9 +43,11 @@ async def _(client, message):
     xxx = await message.reply_sticker("CAACAgUAAxkDAAJJAWhC0UgVeOpk9uRsLK1ZlZHzFfRgAAKLBgAC9v0pVoJkidE43EKFHgQ")
     await asyncio.sleep(3.5)
     await xxx.delete()
-    
-    return await message.reply(msg, reply_markup=InlineKeyboardMarkup(buttons))
-    
+
+    return await message.reply_photo(
+        photo="https://files.catbox.moe/1b37zw.jpg", 
+            
+    return await message.reply_photo(msg, reply_markup=InlineKeyboardMarkup(buttons))
 
 
 @PY.CALLBACK("bahan")
