692a356fdaf66169b21a38d03b8249211307ba83
Not faund <flaelva6@gmail.com>
2025-06-10 20:06:32 +0700

    Create Tqto.py

---
+++ PyroUbot/modules/Tqto.py
@@ -0,0 +1,11 @@
+MODULE = "Tqto"
+HELP = """
+<blockquote> 
+<b>Kaizen banteng</b>
+ <b>Not faund</b>
+<b>Cheel</b>
+<b> kynan</b>
+<b>Team temali</b>
+<b>Tmc parel</b>
+<b>Tmc sugamek</b></blockquote>
+"""
